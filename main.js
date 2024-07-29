3e556425f92e44c4d53c51645d60dfbf49b5f06e
AndreaSdr70 <andrea@sdringola.it>
2024-07-29 16:21:20 +0200

    Video Selfwork - DOM1 - Correzione 2

---
+++ main.js
@@ -0,0 +1,32 @@
+let paragrafi = document.querySelectorAll('p');
+let btnColor = document.querySelector('#btnColor');
+let btnBold = document.querySelector('#btnBold');
+let btnHide = document.querySelector('#btnHide');
+
+
+btnColor.addEventListener('click', ()=> {
+    paragrafi.forEach(paragrafo =>{
+        let r = Math.floor(Math.random()* 256)
+        let g = Math.floor(Math.random()* 256)
+        let b = Math.floor(Math.random()* 256)
+        paragrafo.style.color = `rgb(${r}, ${g}, ${b})`    
+    });
+})
+
+
+btnBold.addEventListener('click', ()=>{
+    paragrafi.forEach(paragrafo =>{
+        paragrafo.style.fontWeight = 'bold'
+    });
+})
+
+
+btnHide.addEventListener('click', ()=>{
+    paragrafi.forEach(paragrafo =>{
+        if(paragrafo.style.display == 'none'){
+            paragrafo.style.display = 'block'
+        }else{
+            paragrafo.style.display = 'none'
+        }
+    });
+})
